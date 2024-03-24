1bc68669aff2b232807c5eefa20b6070760e3a86
nwhang1 <nicholasjwhang@gmail.com>
2024-03-24 09:55:39 -0700

    created switch statements and excluded case-sensitivity

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject? Math, Science, History, English, or Gym?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is important for all parts of our lives, good choice!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science breathes innovation into our lives, great choice!");
+                    break;
+                case "history":
+                    Console.WriteLine("Those who do not study history are doomed to repeat it, good choice!");
+                    break;
+                case "english":
+                    Console.WriteLine("Interesting choice, I'll refrain from commenting further");
+                    break;
+                case "gym":
+                    Console.WriteLine("That's not a subject and you know it...");
+                    break;
+
+            }
         }
     }
 }
